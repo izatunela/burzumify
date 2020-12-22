83917ba3cba06012d93d40a50c42250db7199d1b
Marko Ivanovic <marko.ivanovic.dev@gmail.com>
2020-12-22 23:18:42 +0100

    responsive fix

--- src/Master.vue
+++ src/Master.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-items-center">
-    <div class="fixed h-screen sm:w-1/4 xl:w-1/3 pr-4">
-      <img class="h-full  object-cover " src="../src/assets/img/burzum3.jpg" alt="">
+    <div class="w-0 fixed h-screen sm:w-1/4 xl:w-1/3 pr-4">
+      <img class="w-full h-full object-cover " src="../src/assets/img/burzum3.jpg" alt="">
     </div>
     <div class="w-full sm:w-1/2 xl:w-1/3 mx-auto relative z-30 bg-white">
       <top-panel class="" :title="title"></top-panel>
@@ -10,8 +10,8 @@
         <p class="font-fell ">Conjured by Marko</p>
       </div>
     </div>
-    <div class="fixed h-screen right-0 sm:w-1/4 xl:w-1/3 pl-4">
-      <img class="h-full  object-cover transform -rotate-180" src="../src/assets/img/burzum3.jpg" alt="">
+    <div class="w-0 fixed h-screen right-0 sm:w-1/4 xl:w-1/3 pl-4">
+      <img class="w-full h-full object-cover transform -rotate-180" src="../src/assets/img/burzum3.jpg" alt="">
     </div>
   </div>
 </template>
